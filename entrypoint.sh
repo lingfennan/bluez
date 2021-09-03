76f54b2e2385a411266b8fb3e6cb24ab37053dc9
Ruian Duan <duanruian@gmail.com>
2021-09-03 16:59:59 +0000

    update with latest codeql results

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "Install the dependencies for compiling the repository"
 sed -i~orig -e 's/# deb-src/deb-src/' /etc/apt/sources.list
 apt-get update -yqq
 dpkg --configure -a
-DEBIAN_FRONTEND="noninteractive" apt-get install -yqq tzdata
+DEBIAN_FRONTEND="noninteractive" apt-get install -yqq tzdata python-docutils
 apt-get -yqq build-dep bluez
 ./bootstrap
 ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
